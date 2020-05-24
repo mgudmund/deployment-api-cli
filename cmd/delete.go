7f9e2781dea5bc5decf0685caa4ed88c2c8c08d6
Martin Gudmundsson <martingudmundsson@gmail.com>
2020-05-24 21:40:18 +0200

    Added Delete

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,21 +1,57 @@
 package cmd
 
 import (
-  "fmt"
+	"fmt"
+	"os"
+	"strconv"
 
-  "github.com/spf13/cobra"
+	"github.com/go-resty/resty/v2"
+	"github.com/spf13/cobra"
 )
 
+var DeleteId string
+
 func init() {
-  rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(deleteCmd)
+	deleteCmd.Flags().StringVarP(&DeleteId, "deleteid", "d", "", "Deployment ID to be deleted")
+	deleteCmd.MarkFlagRequired("deleteid")
 }
 
 var deleteCmd = &cobra.Command{
-  Use:   "delete",
-  Short: "Delete a deployment",
-  Long:  `Use this to delete a deployment. Retuns the deployment id that was deleted`,
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("Not implemented")
-  },
+	Use:   "delete",
+	Short: "Delete a deployment",
+	Long:  `Use this to delete a deployment. Retuns the deployment id that was deleted`,
+	Run: func(cmd *cobra.Command, args []string) {
+		deleteDeployment()
+		fmt.Println("Deployment deleted: " + DeleteId)
+	},
 }
 
+func deleteDeployment() {
+	//Get env vars
+	url := os.Getenv("DEPLOYMENT_API_URL")
+	token := os.Getenv("DEPLOYMENT_API_TOKEN")
+
+	client := resty.New()
+	if Verbose {
+		client.SetDebug(true)
+	}
+	// POST JSON string
+	// No need to set content type, if you have client level setting
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetAuthScheme("Bearer").
+		SetAuthToken(token).
+		Delete(url + "/deployments/" + DeleteId)
+
+	if err != nil {
+		fmt.Println("Call to API failed" + err.Error())
+		os.Exit(1)
+	}
+	if resp.IsError() {
+		fmt.Println("HTTP Return Code: " + strconv.Itoa(resp.StatusCode()))
+		fmt.Println(resp.String())
+		os.Exit(1)
+	}
+
+}
